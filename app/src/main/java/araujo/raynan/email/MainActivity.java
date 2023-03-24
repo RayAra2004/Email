024ea916f249893f2f9de4126cd55bd7cabcd669
20211TIIMI0133 <raynan.araujo@gmail.com>
2023-03-24 08:14:46 -0300

    Código Java e comentários

--- app/src/main/java/araujo/raynan/email/MainActivity.java
+++ app/src/main/java/araujo/raynan/email/MainActivity.java
@@ -2,7 +2,14 @@ package araujo.raynan.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Capturando os dados digitados{
+                EditText etEmail = findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto = findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
+                //}
+                //Criando uma intent implícita
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+
+                //Informando quais tipos de app estamos interessados
+                i.setData(Uri.parse("mailto:"));
+
+                //Preenchendo o intent com os dados capturados
+                String[] emails = new String[] {email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                //Tentando executar o Intent
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                }
+                catch (ActivityNotFoundException e){
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
